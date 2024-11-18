bb7ab8cfa96895b9ccf7c8b96bb5475d08111eb2
DinanG <gaoxuedinan@gmail.com>
2024-11-18 13:21:39 -0800

    next is 4.4

--- App.js
+++ App.js
@@ -5,13 +5,36 @@ import cors from "cors";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
+import UserRoutes from "./Kanbas/Users/routes.js";
+import "dotenv/config";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+  })
+);
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET || "kanbas",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+  };
+}
+app.use(session(sessionOptions));
+
 app.use(express.json());
 
 AssignmentRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
+UserRoutes(app);
 Lab5(app);
 app.listen(process.env.PORT || 4000);
