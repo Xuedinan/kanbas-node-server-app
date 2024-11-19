e214157bb53cfb146701be9b57408710fd0f6bc3
DinanG <gaoxuedinan@gmail.com>
2024-11-19 13:35:53 -0800

    update App

--- App.js
+++ App.js
@@ -1,25 +1,33 @@
 import express from "express";
 import session from "express-session";
+import cors from "cors";
+import "dotenv/config";
+
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
-import cors from "cors";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import UserRoutes from "./Kanbas/Users/routes.js";
-import "dotenv/config";
 
 const app = express();
 
+const allowedOrigins = ["http://localhost:3000", process.env.NETLIFY_URL];
 app.use(
   cors({
     credentials: true,
-    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
   })
 );
 
 const sessionOptions = {
-  secret: process.env.SESSION_SECRET || "kanbas", // session 加密密钥
+  secret: process.env.SESSION_SECRET || "kanbas",
   resave: false,
   saveUninitialized: false,
 };
@@ -34,11 +42,19 @@ if (process.env.NODE_ENV !== "development") {
 app.use(session(sessionOptions));
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+console.log("Loading routes...");
 AssignmentRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 UserRoutes(app);
 Lab5(app);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
